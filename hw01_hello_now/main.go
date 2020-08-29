a6fa13b8009730b07342d7d6757b8a77c6ebc080
Maxim Arzamastsev <maxim.arzamastsev@shtormtech.ru>
2020-08-29 12:12:37 +0400

    fix: fixed getting time order

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -9,11 +9,11 @@ import (
 )
 
 func main() {
-	t := time.Now().Truncate(time.Second)
 	result, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 	if err != nil {
 		log.Fatalln("Error with exact time fetching:", err)
 	}
+	t := time.Now().Truncate(time.Second)
 	fmt.Println("current time:", t)
 	fmt.Println("exact time:", result.Truncate(time.Second))
 }
