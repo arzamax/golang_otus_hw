b559d62eff30f4cdc04da323c2fa267d016607ca
Maxim Arzamastsev <maxim.arzamastsev@shtormtech.ru>
2020-09-11 12:44:55 +0400

    feat: completed hw03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,58 @@
 package hw03_frequency_analysis //nolint:golint,stylecheck
 
-func Top10(_ string) []string {
-	// Place your code here
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type wordCounter struct {
+	name  string
+	value int
+}
+
+func getWordCountersDict(arr []string) map[string]int {
+	dict := map[string]int{}
+
+	for _, v := range arr {
+		_, ok := dict[v]
+
+		if ok {
+			dict[v]++
+		} else {
+			dict[v] = 1
+		}
+	}
+
+	return dict
+}
+
+func getSortedWordCounters(dict map[string]int) []wordCounter {
+	counters := make([]wordCounter, 0, len(dict))
+
+	for k, v := range dict {
+		counter := wordCounter{name: k, value: v}
+		counters = append(counters, counter)
+	}
+
+	sort.Slice(counters, func(i, j int) bool {
+		return counters[i].value > counters[j].value
+	})
+
+	return counters
+}
+
+func Top10(str string) []string {
+	arr := strings.Fields(str)
+	dict := getWordCountersDict(arr)
+	counters := getSortedWordCounters(dict)
+	res := make([]string, 0, len(counters))
+
+	for _, v := range counters {
+		res = append(res, v.name)
+		if len(res) == 10 {
+			break
+		}
+	}
+
+	return res
 }
